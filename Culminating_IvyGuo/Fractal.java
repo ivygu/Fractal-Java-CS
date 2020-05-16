2f7be13a621360cf744a51f1fced40eee44a1e15
Ivy Guo <36390263+ivygu@users.noreply.github.com>
2020-05-15 23:55:50 -0400

    Sierpinski Triangle method

--- Culminating_IvyGuo/Fractal.java
+++ Culminating_IvyGuo/Fractal.java
@@ -6,24 +6,53 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
-public class Fractal extends JFrame
+public class Fractal extends Canvas
 {
     JFrame frame;
+    DrawingCanvas canvas; //displays fractal patterns
+
     public Fractal ()
     {
 	frame = new JFrame ("Flavours of Fractals - Fractal Build");
-	
-	
-	
-	
+	canvas = new DrawingCanvas ();
+	frame.getContentPane ().add (canvas);
 	// Set the frame's size and show the frame
 	frame.setSize (600, 500);
 	frame.setVisible (true);
-	
+
     } // Constructor
-    
-    public void paint (Graphics g)
+
+
+    class DrawingCanvas extends JPanel
     {
-    } // paint method
-    
+	public DrawingCanvas ()
+	{
+	    this.setPreferredSize (new Dimension (600, 300));
+	    this.setBackground (Color.white);
+	} //end constructor
+
+	public void paint (Graphics g)
+	{
+	    super.paint (g);
+	    g.setColor (Style.MIDNIGHT_BLUE);
+	    drawSierpinski (g);
+
+	} // paint method
+
+	public void drawSierpinski (Graphics g)
+	{
+	    Polygon basicTriangle = new Polygon ();
+
+	    basicTriangle.addPoint (100, 400);
+	    basicTriangle.addPoint (500, 400);
+	    basicTriangle.addPoint (300, 50);
+
+	    g.fillPolygon (basicTriangle);
+
+	}
+
+	public void drawTriangle ();
+
+
+    } // DrawingCanvas Class
 } // Fractal class
--- Culminating_IvyGuo/Style.java
+++ Culminating_IvyGuo/Style.java
@@ -6,12 +6,45 @@ import javax.swing.*;
 //Constant Stylistic Elements
 public class Style
 {
-    
+
     final static Color
 	FAINT = new Color (244, 245, 250),
 	LIGHT = new Color (179, 188, 245),
 	SEMI = new Color (32, 46, 120),
-	DARK = new Color (0, 6, 57);
+	DARK = new Color (0, 6, 57),
+
+	//Cool Color Scheme (blue)
+	MIDNIGHT_BLUE = new Color (3, 26, 107),
+	YALE_BLUE = new Color (0, 67, 133),
+	AERO_BLUE = new Color (121, 190, 238),
+
+	//Warm Color Scheme (red)
+	SALSA_RED = new Color (199, 0, 57),
+	TULIP_PURPLE = new Color (81, 24, 76),
+	FLAME_ORANGE = new Color (255, 87, 51),
+
+	//Bright Color Scheme
+	GOLDEN_YELLOW = new Color (255, 226, 5),
+	LIME_GREEN = new Color (222, 255, 5),
+	AQUA = new Color (5, 255, 180);
+
+    final static Color[] coolColorList = {
+	MIDNIGHT_BLUE,
+	YALE_BLUE,
+	AERO_BLUE
+	};
+
+    final static Color[] warmColorList = {
+	SALSA_RED,
+	TULIP_PURPLE,
+	FLAME_ORANGE
+	};
+
+    final static Color[] energyColorList = {
+	GOLDEN_YELLOW,
+	LIME_GREEN,
+	AQUA
+	};
 
     final static Font
 	TITLE_FONT = new Font ("Georgia", Font.PLAIN, 50),
